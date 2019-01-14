15663ccc55916e45ccc346cc8b3db65960075088
David Kajpust <kajpustd@gmail.com>
2019-01-14 12:49:45 -0500

    Added in erc20 to get the abi. Contract should compile and source factorry now

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,17 +1,29 @@
 import {} from '@graphprotocol/graph-ts'
 import {
+  NewExchange
+} from '../types/Factory/Factory'
 
-} from '../types/Marketplace/Marketplace'
+import {
+  ERC20Detailed
+} from '../types/ERC20Detailed/ERC20Detailed'
 
 import {
   Factory,
-  Exchange
+  Exchange,
 } from '../types/schema'
 
 
 
-
 export function handleNewExchange(event: NewExchange): void {
+  let id = event.params.exchange.toHex()
+  let exchange = new Exchange(id)
+  exchange.tokenAddress = event.params.token
 
+  // Get the token ticker from calling into the contract,
+  // (token must share ERC20 interface that supports the symbol as public getter)
+  let tokenContract = ERC20Detailed.bind(event.params.token)
+  exchange.tokenSymbol = tokenContract.symbol()
+
+  exchange.save()
 }
 
