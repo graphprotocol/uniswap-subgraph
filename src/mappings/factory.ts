8db98673cdd4e37e2a8cc5b76f91c9685614f49c
David Kajpust <kajpustd@gmail.com>
2019-01-14 12:12:32 -0500

    Codegen works, mapping for exchange can begin

---
+++ src/mappings/factory.ts
@@ -0,0 +1,17 @@
+import {} from '@graphprotocol/graph-ts'
+import {
+
+} from '../types/Marketplace/Marketplace'
+
+import {
+  Factory,
+  Exchange
+} from '../types/schema'
+
+
+
+
+export function handleNewExchange(event: NewExchange): void {
+
+}
+
