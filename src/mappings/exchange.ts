638d0c46e07d99ede10019ed04a5507bff30e96c
David Kajpust <kajpustd@gmail.com>
2019-01-15 11:04:12 -0500

    handleEthPurchase works

--- src/mappings/exchange.ts
+++ src/mappings/exchange.ts
@@ -88,75 +88,75 @@ export function handleTokenPurchase(event: TokenPurchase): void {
 }
 
 export function handleEthPurchase(event: EthPurchase): void {
-  // let exchangeID = event.address.toHex()
-  // let trackedExchange = TrackedExchange.load(exchangeID)
-  //
-  // // if (trackedExchange == null){
-  // //   trackedExchange = new TrackedExchange(exchangeID)
-  // //   trackedExchange.totalEth = BigInt.fromI32(0)
-  // //   trackedExchange.totalToken = BigInt.fromI32(0)
-  // //   trackedExchange.totalUniToken = BigInt.fromI32(0)
-  // //   trackedExchange.totalUsers = 0
-  // // }
-  //
-  // trackedExchange.totalEth = trackedExchange.totalEth.plus(event.params.eth_bought)
-  // trackedExchange.totalToken = trackedExchange.totalToken.minus(event.params.tokens_sold)
-  // trackedExchange.tokenAddress = event.address
-  //
-  // // Because 'token' is not a public getter, we need to derive the name based on the event.address being emitted, so an // if else statement
-  // // let contractAddress = event.address.toHex()
-  // // let userUniTokenID: string
-  // // if (contractAddress == '0x2e642b8d59b45a1d8c5aef716a84ff44ea665914'){
-  // //   trackedExchange.tokenTicker = "BAT"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else if (contractAddress == '0x2c4bd064b998838076fa341a83d007fc2fa50957'){
-  // //   trackedExchange.tokenTicker = "MKR"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else if (contractAddress == '0xae76c84c9262cdb9abc0c2c8888e62db8e22a0bf'){
-  // //   trackedExchange.tokenTicker = "ZRX"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else if (contractAddress == '0x09cabec1ead1c0ba254b09efb3ee13841712be14'){
-  // //   trackedExchange.tokenTicker = "DAI"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else if (contractAddress == '0x4e395304655f0796bc3bc63709db72173b9ddf98'){
-  // //   trackedExchange.tokenTicker = "SPANK"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else if (contractAddress == '0x077d52b047735976dfda76fef74d4d988ac25196'){
-  // //   trackedExchange.tokenTicker = "ANT"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // } else {
-  // //   trackedExchange.tokenTicker = "UNKNOWN"
-  // //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
-  // // }
-  //
-  // let userID = event.params.buyer.toHex()
-  // let user = User.load(userID)
-  // if (user == null){
-  //   user = new User(userID)
+  let exchangeID = event.address.toHex()
+  let trackedExchange = TrackedExchange.load(exchangeID)
+
+  // if (trackedExchange == null){
+  //   trackedExchange = new TrackedExchange(exchangeID)
+  //   trackedExchange.totalEth = BigInt.fromI32(0)
+  //   trackedExchange.totalToken = BigInt.fromI32(0)
+  //   trackedExchange.totalUniToken = BigInt.fromI32(0)
+  //   trackedExchange.totalUsers = 0
   // }
-  //
-  // user.save()
-  //
-  // let userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(event.address.toHex())
-  //
-  // let userExchangeTokenBalance = UserExchangeTokenBalance.load(userUniTokenID)
-  // if (userExchangeTokenBalance == null){
-  //   userExchangeTokenBalance = new UserExchangeTokenBalance(userUniTokenID)
-  //   userExchangeTokenBalance.ethsDeposited = BigInt.fromI32(0)
-  //   userExchangeTokenBalance.tokensDeposited = BigInt.fromI32(0)
-  //   userExchangeTokenBalance.uniTokensOwned = BigInt.fromI32(0)
-  //   userExchangeTokenBalance.userAddress = event.params.buyer
-  //   userExchangeTokenBalance.tokenAddress = event.address
-  //   trackedExchange.totalUsers = trackedExchange.totalUsers + 1
+
+  trackedExchange.totalEth = trackedExchange.totalEth.plus(event.params.eth_bought)
+  trackedExchange.totalToken = trackedExchange.totalToken.minus(event.params.tokens_sold)
+  trackedExchange.tokenAddress = event.address
+
+  // Because 'token' is not a public getter, we need to derive the name based on the event.address being emitted, so an // if else statement
+  // let contractAddress = event.address.toHex()
+  // let userUniTokenID: string
+  // if (contractAddress == '0x2e642b8d59b45a1d8c5aef716a84ff44ea665914'){
+  //   trackedExchange.tokenTicker = "BAT"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else if (contractAddress == '0x2c4bd064b998838076fa341a83d007fc2fa50957'){
+  //   trackedExchange.tokenTicker = "MKR"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else if (contractAddress == '0xae76c84c9262cdb9abc0c2c8888e62db8e22a0bf'){
+  //   trackedExchange.tokenTicker = "ZRX"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else if (contractAddress == '0x09cabec1ead1c0ba254b09efb3ee13841712be14'){
+  //   trackedExchange.tokenTicker = "DAI"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else if (contractAddress == '0x4e395304655f0796bc3bc63709db72173b9ddf98'){
+  //   trackedExchange.tokenTicker = "SPANK"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else if (contractAddress == '0x077d52b047735976dfda76fef74d4d988ac25196'){
+  //   trackedExchange.tokenTicker = "ANT"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
+  // } else {
+  //   trackedExchange.tokenTicker = "UNKNOWN"
+  //   userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(contractAddress)
   // }
-  //
-  // trackedExchange.save()
-  //
-  //
-  // userExchangeTokenBalance.ethsDeposited = userExchangeTokenBalance.ethsDeposited.plus(event.params.eth_bought)
-  // userExchangeTokenBalance.tokensDeposited = userExchangeTokenBalance.tokensDeposited.minus(event.params.tokens_sold)
-  //
-  // userExchangeTokenBalance.save()
+
+  let userID = event.params.buyer.toHex()
+  let user = User.load(userID)
+  if (user == null){
+    user = new User(userID)
+  }
+
+  user.save()
+
+  let userUniTokenID = trackedExchange.tokenTicker.concat('-').concat(event.address.toHex())
+
+  let userExchangeTokenBalance = UserExchangeTokenBalance.load(userUniTokenID)
+  if (userExchangeTokenBalance == null){
+    userExchangeTokenBalance = new UserExchangeTokenBalance(userUniTokenID)
+    userExchangeTokenBalance.ethsDeposited = BigInt.fromI32(0)
+    userExchangeTokenBalance.tokensDeposited = BigInt.fromI32(0)
+    userExchangeTokenBalance.uniTokensOwned = BigInt.fromI32(0)
+    userExchangeTokenBalance.userAddress = event.params.buyer
+    userExchangeTokenBalance.tokenAddress = event.address
+    trackedExchange.totalUsers = trackedExchange.totalUsers + 1
+  }
+
+  trackedExchange.save()
+
+
+  userExchangeTokenBalance.ethsDeposited = userExchangeTokenBalance.ethsDeposited.plus(event.params.eth_bought)
+  userExchangeTokenBalance.tokensDeposited = userExchangeTokenBalance.tokensDeposited.minus(event.params.tokens_sold)
+
+  userExchangeTokenBalance.save()
 }
 
 // will be handled First, so user , and its token balance may not exist
@@ -225,7 +225,6 @@ export function handleAddLiquidity(event: AddLiquidity): void {
   }
 
   trackedExchange.save()
-
 
   userExchangeTokenBalance.ethsDeposited = userExchangeTokenBalance.ethsDeposited.plus(event.params.eth_amount)
   userExchangeTokenBalance.tokensDeposited = userExchangeTokenBalance.tokensDeposited.plus(event.params.token_amount)
